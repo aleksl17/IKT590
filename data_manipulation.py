d65f036506ab9798a7cfbe727f3442ca11bbc7e9
aleksl17 <aleksl17@uia.no>
2022-01-19 14:50:26 +0100

    Improved import data. Now returns list of pandas DataFrames for easier processing later on. Added interpolate inital upload.

--- data_manipulation.py
+++ data_manipulation.py
@@ -1,5 +1,25 @@
+import logging
 import pandas
+import numpy
+
+import interpolate
+
 
 def manipulate_data(file, interval=1440):
+    """Manipulates data"""
+    
+    # Initalize logger
+    logger = logging.getLogger(__name__)
+    
+    # Variables
+    x = numpy.array([])
+    y = numpy.array([])
+
     data = pandas.read_csv(file)
+    print(data)
+    print(type(data))
+
+    interpolate.interpolate(data)
+
+    print(data)
     
