23474370a0f5db824f3b0449dfb8d4997a40a041
aleksl17 <aleksl17@uia.no>
2022-01-28 09:44:27 +0100

    Moved data scripts to designated folder

---
+++ DataScripts/ScriptsMain.py
@@ -0,0 +1,34 @@
+import logging
+import time
+import os
+
+import import_data
+import data_manipulation
+
+
+def main():
+    # Initialize logging
+    if not os.path.exists('./.logs'):
+        os.makedirs('./.logs')
+    currentTime = str(int(time.time()))
+    logFile = os.path.join('./.logs/' + currentTime + '.log')
+    logging.basicConfig(filename=logFile, format='%(asctime)s %(levelname)s %(name)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S', level=logging.DEBUG)
+    
+    logging.info('Started')
+    ################ ======== Functions/code goes below here ======== ################
+    
+    # Download data via API if .localData folder is empty
+    # import_data.import_data(signalFrom="2021-01-01T01:00:00.000Z", signalTo="2022-01-01T01:00:00.000Z")
+    idd = import_data.import_data()
+    print(idd)
+
+    # Create dataset from local data files
+    # for file in os.listdir('./.localData'):
+    #     data_manipulation.manipulate_data(f"./.localData/{file}")
+    
+    ################ ======== Functions/code goes above here ======== ################
+    logging.info('Finished')
+
+
+if __name__ == "__main__":
+    main()
