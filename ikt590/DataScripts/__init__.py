7c5b22ddc41c017d072aaacff3ba6c90e2185d7f
aleksl17 <aleksl17@uia.no>
2022-02-02 10:12:33 +0100

    Moved KMeans() inside main(), now inherets currentTime. Fixed package importing.

--- ikt590/DataScripts/__init__.py
+++ ikt590/DataScripts/__init__.py
@@ -1,4 +1,4 @@
 # __all__ = ['data_manipulation', 'data_import', 'interpolate']
-from DataScripts.data_manipulation import manipulate_data
-from DataScripts.data_import import import_data
-from DataScripts.interpolate import interpolation
+# from DataScripts.data_manipulation import manipulate_data
+# from DataScripts.data_import import import_data
+# from DataScripts.interpolate import interpolation
