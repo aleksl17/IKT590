46b7d614b34a12772c5036e24304fa05c7c961aa
aleksl17 <aleksl17@uia.no>
2022-02-02 11:30:28 +0100

    Improved datman code

--- ikt590/DataScripts/data_manipulation.py
+++ ikt590/DataScripts/data_manipulation.py
@@ -6,7 +6,7 @@ import os
 import data_interpolate
 
 
-def manipulate_data(csvDirectory='./.localData', interval=1440):
+def manipulate_data(csvDirectory='./.localData', interval=1440, interpolate=True):
     """Manipulates data"""
     
     # Initalize logger
@@ -21,13 +21,10 @@ def manipulate_data(csvDirectory='./.localData', interval=1440):
     # Creates list of DataFrames from data from CSV files in csvDirectory
     for file in os.listdir(csvDirectory):
         csvData = pandas.read_csv(os.path.join(csvDirectory, file))
+        if interpolate:
+            csvData = data_interpolate.interpolation(csvData)
         dataList.append(csvData)
 
-    # TODO: move code into one loop, the loop above
-    for listData in dataList:
-        interpolatedData = data_interpolate.interpolation(listData)
-        new_y.append(interpolatedData)
-    
     print(new_y[0])
     logger.debug(type(new_y[0]))
     logger.debug(len(new_y[0]))
