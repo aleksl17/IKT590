8df04616f2235610c287fd4de7b33440558d44fc
aleksl17 <aleksl17@uia.no>
2022-02-04 14:41:57 +0100

    mess

--- ikt590/DataScripts/data_manipulation.py
+++ ikt590/DataScripts/data_manipulation.py
@@ -1,12 +1,19 @@
 import logging
+from sqlite3 import Timestamp
 import pandas
 import numpy
 import os
 
 import data_interpolate
 
+# TODO:
+# Function creates CSV with 2D list. Where each row is a subset of index 0 = start datetime, and 1... is values of interval
+#   [
+#   [2021-01-01:00:11:22, 1.4, 1.5, 1.6, ... ],
+#   [2022-02-02:12:34:45, 9.9, 5.4, 7.1, ... ]
+#   ]
 
-def manipulate_data(csvDirectory='./.localData', interval=1440, interpolate=True):
+def manipulate_data(csvDirectory='./.localData', interval="24H", interpolate=False):
     """Manipulates data"""
     
     # Initalize logger
@@ -14,18 +21,35 @@ def manipulate_data(csvDirectory='./.localData', interval=1440, interpolate=True
     
     # Variables
     dataList = []
-    new_y = []
     x = numpy.array([])
     y = numpy.array([])
 
     # Creates list of DataFrames from data from CSV files in csvDirectory
     for file in os.listdir(csvDirectory):
+        # csvData = pandas.read_csv(os.path.join(csvDirectory, file), parse_dates=['timestamp'])
         csvData = pandas.read_csv(os.path.join(csvDirectory, file))
+        # csvData['Datetime']
         if interpolate:
             csvData = data_interpolate.interpolation(csvData)
-        dataList.append(csvData)
+        print(csvData['value'].values)
 
-    print(new_y[0])
-    logger.debug(type(new_y[0]))
-    logger.debug(len(new_y[0]))
-    logger.debug(new_y[0])
+        # How to convert Pandas DataFrame to Pandas Series. Might not be needed here, but good to know how.
+        # Requires "timestamp" to be DateTime and not string.
+        # csvDataSeries = pandas.Series(data=csvData['value'].values, index=csvData['timestamp'])
+
+        # csvDataX, csvDataY = data_interpolate.interpolation(csvData)
+        # print(csvDataX)
+        # print(csvDataY)
+        
+        # csvData = csvData.groupby(pandas.Grouper(key='timestamp', freq='1D'))
+        # print(csvData)
+        
+        dataList.append(csvData)
+    
+    
+
+
+    # print(dataList[0])
+    # logger.debug(type(dataList[0]))
+    # logger.debug(len(dataList[0]))
+    # logger.debug(dataList[0])
