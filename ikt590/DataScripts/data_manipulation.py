d3d004f8fdb5a6013eca2a014e3c32a442b39680
aleksl17 <aleksl17@uia.no>
2022-02-08 00:43:18 +0100

    data_manipulatiuon now works as intended. Saving to file to be implemented.

--- ikt590/DataScripts/data_manipulation.py
+++ ikt590/DataScripts/data_manipulation.py
@@ -1,17 +1,20 @@
 import logging
-from sqlite3 import Timestamp
 import pandas
 import numpy
+import math
 import os
 
 import data_interpolate
 
 # TODO:
+# Doesn't work if <interpolate=False>
 # Function creates CSV with 2D list. Where each row is a subset of index 0 = start datetime, and 1... is values of interval
 #   [
 #   [2021-01-01:00:11:22, 1.4, 1.5, 1.6, ... ],
 #   [2022-02-02:12:34:45, 9.9, 5.4, 7.1, ... ]
 #   ]
+# Expected Shape: [<floor of length of data times sample_size>, <sample_size>]
+
 
 def manipulate_data(csvDirectory='./.localData', sample_size=40, interpolate=True):
     """Manipulates data"""
@@ -20,42 +23,25 @@ def manipulate_data(csvDirectory='./.localData', sample_size=40, interpolate=Tru
     logger = logging.getLogger(__name__)
     
     # Variables
-    datasetList = numpy.array([])
-    dataset = numpy.array([])
+    datasetList = []
+    dataset = numpy.empty((0))
     # x = numpy.array([])
     # y = numpy.array([])
 
     # Creates list of DataFrames from data from CSV files in csvDirectory
     for file in os.listdir(csvDirectory):
-        # csvData = pandas.read_csv(os.path.join(csvDirectory, file), parse_dates=['timestamp'])
+        # Read files and interpolate if "interpolate==True"
         csvData = pandas.read_csv(os.path.join(csvDirectory, file))
-        # csvData['Datetime']
         if interpolate:
             csvData = data_interpolate.interpolation(csvData)
         
-        for set in range(len(csvData)-sample_size):
-            print(set)
-            numpy.append(dataset, csvData[set:set+sample_size])
+        # Split csvData into samples (Now without loops!)
+        splitAmount = math.floor(len(csvData)/sample_size)
+        trimIndex = splitAmount*sample_size
+        csvData = csvData[:trimIndex]
+        dataset = numpy.split(csvData, splitAmount)
 
-        # How to convert Pandas DataFrame to Pandas Series. Might not be needed here, but good to know how.
-        # Requires "timestamp" to be DateTime and not string.
-        # csvDataSeries = pandas.Series(data=csvData['value'].values, index=csvData['timestamp'])
-
-        # csvDataX, csvDataY = data_interpolate.interpolation(csvData)
-        # print(csvDataX)
-        # print(csvDataY)
-        
-        # csvData = csvData.groupby(pandas.Grouper(key='timestamp', freq='1D'))
-        # print(csvData)
-        
-        numpy.append(datasetList, dataset)
+        # Append dataset containing samples to list
+        datasetList.append(dataset)
     
-print(manipulate_data())
-    
-    
-
-
-    # print(dataList[0])
-    # logger.debug(type(dataList[0]))
-    # logger.debug(len(dataList[0]))
-    # logger.debug(dataList[0])
+    return datasetList
