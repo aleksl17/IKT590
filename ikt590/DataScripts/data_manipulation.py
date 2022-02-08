230a6c9eacc9e6f886d17046a54cdb89152b9ed2
aleksl17 <aleksl17@uia.no>
2022-02-08 15:08:44 +0100

    Updated .gitignore and data_interpolate scripts. Added create_dataset and read_dataset functions.

--- ikt590/DataScripts/data_manipulation.py
+++ ikt590/DataScripts/data_manipulation.py
@@ -1,47 +1,67 @@
 import logging
 import pandas
-import numpy
-import math
+import time
+import json
 import os
 
 import data_interpolate
 
 # TODO:
-# Doesn't work if <interpolate=False>
-# Function creates CSV with 2D list. Where each row is a subset of index 0 = start datetime, and 1... is values of interval
-#   [
-#   [2021-01-01:00:11:22, 1.4, 1.5, 1.6, ... ],
-#   [2022-02-02:12:34:45, 9.9, 5.4, 7.1, ... ]
-#   ]
-# Expected Shape: [<floor of length of data times sample_size>, <sample_size>]
+# Optimization: Use Numpy arrays instead of Python lists
 
 
-def manipulate_data(csvDirectory='./.localData', sample_size=40, interpolate=True):
+def create_dataset(inputDirectory='./.localData/', outputDirectory='./.dataset/', sample_size=40):
     """Manipulates data"""
     
     # Initalize logger
     logger = logging.getLogger(__name__)
     
     # Variables
+    currentTime = str(int(time.time()))
     datasetList = []
-    dataset = numpy.empty((0))
-    # x = numpy.array([])
-    # y = numpy.array([])
+    metadata = []
+    samples = []
 
-    # Creates list of DataFrames from data from CSV files in csvDirectory
-    for file in os.listdir(csvDirectory):
-        # Read files and interpolate if "interpolate==True"
-        csvData = pandas.read_csv(os.path.join(csvDirectory, file))
-        if interpolate:
-            csvData = data_interpolate.interpolation(csvData)
+    # Creates list of DataFrames from data from CSV files input directory
+    for file in os.listdir(inputDirectory):
+        # Read files and interpolate
+        csvData = pandas.read_csv(os.path.join(inputDirectory, file))
+        data = data_interpolate.interpolation(csvData)
+        data = data.tolist()
+
+        logger.debug(type(data))
+        # logger.debug(data)
+        logger.debug(len(data))
+        # logger.debug(numpy.shape(data))
         
-        # Split csvData into samples (Now without loops!)
-        splitAmount = math.floor(len(csvData)/sample_size)
-        trimIndex = splitAmount*sample_size
-        csvData = csvData[:trimIndex]
-        dataset = numpy.split(csvData, splitAmount)
+        # Split data into samples with overlap and create respective metadata tabel
+        for set in range(len(data)-sample_size):
+            samples.append(data[set:set+sample_size])
+            metadata.append([file.split('.csv')[0], csvData['timestamp'][set], data[set]])
 
-        # Append dataset containing samples to list
-        datasetList.append(dataset)
-    
-    return datasetList
+        # Create list of datasets
+        datasetList.append([metadata, samples])
+        
+        # Create ouput directory and write dataset to JSON file in said directory
+        if not os.path.exists(outputDirectory):
+            os.makedirs(outputDirectory)
+        with open(os.path.join(outputDirectory+'dataset-'+currentTime+'.json'), 'w') as filehandle:
+            filehandle.write(json.dumps(datasetList))
+
+
+def read_dataset(datasetFile='./.dataset/datasetV1.0.json', returnType='list'):
+    """Reads data from file and returns it"""
+
+    # Variables
+    datasetList = []
+
+    # Opens dataset file and reads content to a python list
+    with open(datasetFile, 'r') as filehandle:
+        datasetList = json.loads(filehandle.read())
+
+    # Return dataset in preferred format
+    if returnType=='list':
+        return datasetList
+    else:
+        print(f"Invalid \"returnType\": {returnType}")
+        return
