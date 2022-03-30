c3c54e815ad5f2fd6475fe2c5071329408bfae8f
aleksl17 <aleksl17@uia.no>
2022-03-30 16:28:08 +0200

    Added all data fit

--- ikt590/autoencoder_train_main.py
+++ ikt590/autoencoder_train_main.py
@@ -15,11 +15,13 @@ os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 def main():
     # Variables
     batch_size = 10000
-    batch_epoch = 1000
-    batches = 100
+    batch_epoch = 100
+    batches = 10
+    epochs = 250
     sample_size = 40
     output_size = 3
     load_old_models = False
+    batch = False
     loss_list = []
     dataset = []
     x = []
@@ -46,20 +48,28 @@ def main():
     model.add(decoder)
     model.compile(optimizer='adam', loss='mse')
 
-    for i in range(batches):
-        print('_________________________________________________________________')
-        print(f'Training for batch: {i} of {batches}')
-        print(f'Total training epochs: {i * batch_epoch}. Total training data: {i*batch_size}')
-        print(f'Time: {datetime.datetime.now()}')
-        x0 = np.array(random.sample(x,batch_size))
-        history = model.fit(x0.reshape(x0.shape[0], x0.shape[1], 1),x0, epochs=batch_epoch, shuffle=True, verbose=1)
-        loss = sum(history.history['loss']) / len(history.history['loss'])
+    if batch:
+        # Batches
+        for i in range(batches):
+            print('_________________________________________________________________')
+            print(f'Training for batch: {i} of {batches}')
+            print(f'Total training epochs: {i * batch_epoch}. Total training data: {i*batch_size}')
+            print(f'Time: {datetime.datetime.now()}')
 
-        print(f'Loss for batch {i}: {loss }')
-        loss_list.append(loss)
+            x0 = np.array(random.sample(x,batch_size))
+            history = model.fit(x0.reshape(x0.shape[0], x0.shape[1], 1),x0, epochs=batch_epoch, shuffle=True, verbose=1)
+            loss = sum(history.history['loss']) / len(history.history['loss'])
 
-        print(f'Saving models for batch: {i}')
-        encoder.save('models/encoder')
+            print(f'Loss for batch {i}: {loss }')
+            print(f'Saving models for batch: {i}')
+            
+            encoder.save('models/encoder')
+            decoder.save('models/decoder')
+    else:
+        # All data
+        x = np.asarray(x)
+        model.fit(x.reshape(x.shape[0], x.shape[1], 1), x, epochs=epochs, shuffle=True, verbose=1)
+        encoder.save('mpdels/encoder')
         decoder.save('models/decoder')
 
 
